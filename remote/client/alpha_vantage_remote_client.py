b98a83da586b61497c0af78aba3953b19e663b5d
Estêvão Fáy <estevaofay@getmentum.com>
2022-11-14 17:57:06 -0300

    feat(alpha-vantage): add alpha vantage as a data provider
    add AlphaVantageDataProvider and AlphaVantageRemoteClient alongside relevant environment changes to make it work

--- remote/client/alpha_vantage_remote_client.py
+++ remote/client/alpha_vantage_remote_client.py
@@ -1,6 +1,15 @@
+from alpha_vantage.timeseries import TimeSeries
+
+from core.environment_variables import settings
 from remote.client.remote_client import RemoteClient
 
 
-class GoogleRemoteClient(RemoteClient):
+class AlphaVantageRemoteClient(RemoteClient):
+
+    def __init__(self):
+        self.api_key = settings.ALPHA_VANTAGE_API_KEY
+
     def get_stock_price_information(self, ticker: str):
-        pass
+        time_series: TimeSeries = TimeSeries(key=settings.ALPHA_VANTAGE_API_KEY)
+        data, _ = time_series.get_quote_endpoint(symbol=ticker)
+        return data
