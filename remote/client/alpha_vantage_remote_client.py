06f8274e89f0556a2092c9425a9b30e7422f55c7
Leonardo <118088717+leogabrielps@users.noreply.github.com>
2022-11-22 19:57:40 -0300

    Update remote/client/alpha_vantage_remote_client.py
    Co-authored-by: Estêvão Francisco Fáy <estevao@getmentum.com>

--- remote/client/alpha_vantage_remote_client.py
+++ remote/client/alpha_vantage_remote_client.py
@@ -11,7 +11,7 @@ class AlphaVantageRemoteClient(RemoteClient):
 
     def get_stock_price_information(self, ticker: str):
         time_series: TimeSeries = TimeSeries(key=settings.ALPHA_VANTAGE_API_KEY)
-        fundamental_Data: FundamentalData = FundamentalData(key=settings.ALPHA_VANTAGE_API_KEY)
+        fundamental_company_data: FundamentalData = FundamentalData(key=settings.ALPHA_VANTAGE_API_KEY)
         data, _ = time_series.get_quote_endpoint(symbol=ticker)
         dataCompany, _ = fundamental_Data.get_company_overview(symbol=ticker)
         data['11. currency'] = dataCompany['Currency']
